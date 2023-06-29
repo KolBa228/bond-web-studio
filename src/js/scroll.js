5e676b1f353580ece19e6fdb3f0ed3a4bb7d3855
unknown <jack22toplolkek@gmail.com>
2023-06-29 21:08:46 +0300

    add animated scroll and links

---
+++ src/js/scroll.js
@@ -0,0 +1,20 @@
+const box = document.querySelectorAll('.scroll-check');
+
+window.addEventListener('scroll', checkImages);
+
+checkImages();
+
+function checkImages() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  box.forEach(item => {
+    const itemTop = item.getBoundingClientRect().top;
+
+    if (itemTop < triggerBottom) {
+      item.classList.add('scroll-check');
+    } else {
+      item.classList.remove('scroll-check');
+      console.log(123);
+    }
+  });
+}
