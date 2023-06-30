64bc8ab3f3cf0b1b773cf71ccd87bfb5ae610cbb
Anna Samoilova <samoilova0299@gmail.com>
2023-06-30 23:42:49 +0200

    add slider

---
+++ src/js/create-our-cases-gallery.js
@@ -0,0 +1,26 @@
+import { galleryCases } from '../js/our-cases-gallery';
+
+const listCases = document.querySelector('.our-cases-list');
+
+export function galleryMarkup() {
+  let markup = '';
+
+  galleryCases.forEach(caseItem => {
+    const { title, description, largeImage, smallImage } = caseItem;
+
+    const itemMarkup = `
+      <li class="our-cases-card swiper-slide">
+        <img class="cases-big-img" src="${largeImage}" alt="${title}" >
+        <img class="cases-small-img" src="${smallImage}" alt="${title}">
+        <h3 class="cases-card-title">${title}</h3>
+        <p class="cases-card-text">${description}</p>
+      </li>
+    `;
+
+    markup += itemMarkup;
+  });
+
+  listCases.insertAdjacentHTML('beforeend', markup);
+}
+
+export { listCases };
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { galleryMarkup } from './js/create-our-cases-gallery';
+import { listCases } from './js/create-our-cases-gallery';
+import { swiper } from './js/swiper';
+galleryMarkup();
