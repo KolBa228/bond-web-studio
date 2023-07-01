4e8e72d1c35c0fef33036c9478f939888d0689bf
KolBa228 <technotrashinfo@gmail.com>
2023-07-01 21:12:54 +0100

    up10

--- src/js_2/get_in_touch_with_us.js
+++ src/js_2/get_in_touch_with_us.js
@@ -53,10 +53,60 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.contact-form');
+// 
 const iName = document.querySelector('#fname');
+iName.addEventListener('input', function() {
+  const inputValue = iName.value.trim();
+  
+  if (inputValue.length < 3 || !/^[a-zA-Z]+$/.test(inputValue)) {
+    iName.setCustomValidity('The name must be at least 3 letters without spaces or other symbols.');
+  } else {
+    iName.setCustomValidity('');
+  }
+});
+// 
+// 
 const iPhone = document.querySelector('#phone');
+
+iPhone.addEventListener('input', function() {
+  const inputValue = iPhone.value.trim();
+  
+  if (!/^\d{9}$/.test(inputValue)) {
+    iPhone.setCustomValidity('Please enter a valid English mobile number (9 digits).');
+  } else {
+    iPhone.setCustomValidity('');
+  }
+});
+// 
+// 
 const iEmail = document.querySelector('#email');
+
+iEmail.addEventListener('input', function() {
+  const inputValue = iEmail.value.trim();
+  
+  // Regular expression for email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  if (!emailRegex.test(inputValue)) {
+    iEmail.setCustomValidity('Please enter a valid email address.');
+  } else {
+    iEmail.setCustomValidity('');
+  }
+});
+// 
+// 
 const textarea = document.querySelector('.comment');
+
+textarea.addEventListener('input', function() {
+  const inputValue = textarea.value.trim();
+  
+  if (inputValue.length < 20) {
+    textarea.setCustomValidity('Please enter at least 20 characters.');
+  } else {
+    textarea.setCustomValidity('');
+  }
+});
+// 
 const input = document.querySelector('input');
 
 form.addEventListener('submit', onFormSubmit);
@@ -105,4 +155,5 @@ function textareaInputMessage() {
 }
 
 textareaInputMessage();
+
 
