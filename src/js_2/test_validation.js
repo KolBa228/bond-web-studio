9fb8f7cbec19ee94564bd21e3976d6ddcecda511
KolBa228 <technotrashinfo@gmail.com>
2023-07-04 16:57:59 +0100

    up14

--- src/js_2/test_validation.js
+++ src/js_2/test_validation.js
@@ -1,344 +1,5 @@
 
-// // // // textareaInputMessage();
-
-// // // import { functions } from 'lodash';
-// // import throttle from 'lodash.throttle';
-
-// // const form = document.querySelector('.contact-form');
-// // const iName = document.querySelector('#fname');
-// // const iPhone = document.querySelector('#phone');
-// // const iEmail = document.querySelector('#email');
-// // const textarea = document.querySelector('.comment');
-// // const submitButton = document.querySelector('.sub-btn');
-// // const errorMessage = document.querySelector('#error-message');
-// // const STORAGE_KEY = 'feedback-form-state';
-
-// // const nameErr = document.querySelector('.name-err');
-
-// // const commErr = document.querySelector('.comment-err');
-
-// // function testErrN() {
-// //     if (iName.value.length >= 3) {
-// //         unShowNameErr()
-// //     } else {
-// //         showNameErr()
-// //     }
-// // };
-
-// // function showNameErr() {
-// //     if (iName.value.length <= 3) {
-// //         nameErr.style.display = "unset";
-// //     }
-// // };
-
-// // function unShowNameErr() {
-// //     if (iName.value.length <= 3) {
-// //         nameErr.style.display = "none";
-// //     }
-// // };
-
-// // function showCommentErr() {
-// //     if (textarea.value.length <= 20) {
-// //         commErr.style.display = "unset";
-// //     }
-// // };
-
-// // function unShowCommentErr() {
-// //     if (textarea.value.length <= 20) {
-// //         commErr.style.display = "none";
-// //     }
-// // };
-
-// // form.addEventListener('submit', onFormSubmit);
-// // form.addEventListener('input', throttle(onFormInput, 500));
-
-// // let feedbackFormData = {};
-
-// // function onFormInput(e) {
-// //     e.preventDefault();
-
-// //     testErrN()
-   
-// //   feedbackFormData[e.target.name] = e.target.value;
-
-// //   const data = JSON.stringify(feedbackFormData);
-
-// //   localStorage.setItem(STORAGE_KEY, data);
-
-// //   isBtnDisabled();
-  
-// //   if (e.target === textarea) {
-// //     feedbackFormData.message = e.target.value;
-// //   }
-// // }
-
-// // function onFormSubmit(e) {
-// //   e.preventDefault();
-// //     isBtnDisabledTest();
-// //   e.currentTarget.reset();
-
-// //   localStorage.removeItem(STORAGE_KEY);
-
-// //   feedbackFormData = {};
-
-// // //   form.submit();
-// // }
-
-// // function textareaInputMessage() {
-// //   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-// //   if (savedData && savedData.email) {
-// //     iEmail.value = savedData.email;
-// //     feedbackFormData.email = savedData.email;
-// //   }
-
-// //   if (savedData && savedData.message) {
-// //     textarea.value = savedData.message;
-// //     feedbackFormData.message = savedData.message;
-// //   }
-// // }
-
-// // function isBtnDisabled() {
-// //   if (
-// //     iName.value.length >= 3 &&
-// //     iPhone.value.length >= 9 &&
-// //     iEmail.value.length >= 3 &&
-// //     textarea.value.length >= 20
-// //   ) {    submitButton.disabled = false;
-// //   } else {
-// //       submitButton.disabled = true;
-// //   }
-// // }
-
-// // // TEST
-
-
-// // function isBtnDisabledTest() {
-// //   if (
-// //     iName.value.length < 3 &&
-// //     iPhone.value.length < 9 &&
-// //     iEmail.value.length < 3 &&
-// //     textarea.value.length < 20
-// //   ) {
-// //       return
-// //   }
-  
-// // };
-
-// // // ВАЛИДАЦИЯ ИМЕНИ
-// // iName.addEventListener('input', validateName);
-
-// // function validateName() {
-// //   const name = iName.value;
-// //   const pattern = /^[A-Za-z]{3,}$/;
-
-// //     if (!pattern.test(name)) {
-// //     iName.setCustomValidity('Enter a name consisting only of letters and with a minimum length of 3 characters.');
-// //   } else {
-// //     iName.setCustomValidity('');
-// //   }
-// // }
-
-// // // ВАЛИДАЦИЯ EMAIL
-// // iEmail.addEventListener('input', validateEmail);
-
-// // function validateEmail() {
-// //   const email = iEmail.value;
-// //   const pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-
-// //   if (!pattern.test(email)) {
-// //     iEmail.setCustomValidity('Enter a valid email address.');
-// //   } else {
-// //     iEmail.setCustomValidity('');
-// //   }
-// // }
-
-// // // ВАЛИДАЦИЯ НОМЕРА
-// // iPhone.addEventListener('input', validatePhone);
-
-// // function validatePhone() {
-// //   const phone = iPhone.value;
-// //   const pattern = /^[0-9]{9,18}$/;
-
-// //   if (!pattern.test(phone)) {
-// //     iPhone.setCustomValidity('Enter a number consisting only of digits and with a minimum length of 9 characters.');
-// //   } else {
-// //     iPhone.setCustomValidity('');
-// //   }
-// // }
-
-// // // isBtnDisabled();
-// // textareaInputMessage();
-
-
-// import throttle from 'lodash.throttle';
-
-// const form = document.querySelector('.contact-form');
-// const iName = document.querySelector('#fname');
-// const iPhone = document.querySelector('#phone');
-// const iEmail = document.querySelector('#email');
-// const textarea = document.querySelector('.comment');
-// const submitButton = document.querySelector('.sub-btn');
-// const errorMessage = document.querySelector('#error-message');
-// const STORAGE_KEY = 'feedback-form-state';
-
-// const nameErr = document.querySelector('.name-err');
-
-// function testErrN() {
-//   if (iName.value.length >= 3) {
-//     unShowNameErr();
-//   } else {
-//     showNameErr();
-//   }
-// }
-
-// function showNameErr() {
-//   if (iName.value.length <= 3) {
-//     nameErr.style.display = "unset";
-//   }
-// }
-
-// function unShowNameErr() {
-//   if (iName.value.length <= 3) {
-//     nameErr.style.display = "none";
-//   }
-// }
-
-// form.addEventListener('submit', onFormSubmit);
-// form.addEventListener('input', throttle(onFormInput, 500));
-
-// let feedbackFormData = {};
-
-// function onFormInput(e) {
-//   e.preventDefault();
-
-//   unShowNameErr();
-//   testErrN();
-
-//   feedbackFormData[e.target.name] = e.target.value;
-
-//   const data = JSON.stringify(feedbackFormData);
-
-//   localStorage.setItem(STORAGE_KEY, data);
-
-//   isBtnDisabled();
-
-//   if (e.target === textarea) {
-//     feedbackFormData.message = e.target.value;
-//   }
-// }
-
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   isBtnDisabledTest();
-//   e.currentTarget.reset();
-
-//   localStorage.removeItem(STORAGE_KEY);
-
-//   feedbackFormData = {};
-
-// }
-
-// function textareaInputMessage() {
-//   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-//   if (savedData && savedData.email) {
-//     iEmail.value = savedData.email;
-//     feedbackFormData.email = savedData.email;
-//   }
-
-//   if (savedData && savedData.message) {
-//     textarea.value = savedData.message;
-//     feedbackFormData.message = savedData.message;
-//   }
-// }
-
-// function isBtnDisabled() {
-//   if (
-//     iName.value.length >= 3 &&
-//     iPhone.value.length >= 9 &&
-//     iEmail.value.length >= 3 &&
-//     textarea.value.length >= 20
-//   ) {
-//     submitButton.disabled = false;
-//   } else {
-//     submitButton.disabled = true;
-//   }
-// }
-
-// function isBtnDisabledTest() {
-//   if (
-//     iName.value.length < 3 ||
-//     iPhone.value.length < 9 ||
-//     iEmail.value.length < 3 ||
-//     textarea.value.length < 20
-//   ) {
-//     return;
-//   }
-// }
-
-// // ВАЛИДАЦИЯ ИМЕНИ
-// iName.addEventListener('input', validateName);
-
-// function validateName() {
-//   const name = iName.value;
-//   const pattern = /^[A-Za-z]{3,}$/;
-
-//   if (!pattern.test(name)) {
-//     iName.setCustomValidity('Enter a name consisting only of letters and with a minimum length of 3 characters.');
-//   } else {
-//     iName.setCustomValidity('');
-//   }
-// }
-
-// // ВАЛИДАЦИЯ EMAIL
-// iEmail.addEventListener('input', validateEmail);
-
-// function validateEmail() {
-//   const email = iEmail.value;
-//   const pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-
-//   if (!pattern.test(email)) {
-//     iEmail.setCustomValidity('Enter a valid email address.');
-//   } else {
-//     iEmail.setCustomValidity('');
-//   }
-// }
-
-// // ВАЛИДАЦИЯ НОМЕРА
-
-// iPhone.addEventListener('keydown', restrictNonNumericInput);
-// iPhone.addEventListener('input', cleanNonNumericInput);
-// iPhone.addEventListener('input', validatePhone);
-
-// function restrictNonNumericInput(event) {
-//   const allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', 'Backspace'];
-//   const keyPressed = event.key;
-  
-//   if (!allowedKeys.includes(keyPressed)) {
-//     event.preventDefault();
-//   }
-// }
-
-// function cleanNonNumericInput() {
-//   let phone = iPhone.value;
-//   phone = phone.replace(/[^0-9+]/g, ''); // Удалить все символы, кроме цифр и "+"
-//   iPhone.value = phone;
-// }
-
-// function validatePhone() {
-//   let phone = iPhone.value;
-//   const pattern = /^[0-9]{9,18}$/;
-
-//   if (!pattern.test(phone)) {
-//     iPhone.setCustomValidity('Enter a number consisting only of digits and with a minimum length of 9 characters.');
-//   } else {
-//     iPhone.setCustomValidity('');
-//   }
-// }
-
-// textareaInputMessage();
-
+import { functions } from 'lodash';
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.contact-form');
@@ -352,25 +13,46 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const nameErr = document.querySelector('.name-err');
 
+const commErr = document.querySelector('.comment-err');
+
+const totalErr = document.querySelector('.btn-err');
+
+function showTotalErr() {
+    if (textarea.value.length < 20 || iName.value.length < 3 || iPhone.value.length < 9 || iEmail.value.length < 3) {
+    totalErr.style.display = "unset";
+    } else {
+        totalErr.style.display = "none"
+    }
+};
+
+function showCommentErr() {
+    if (textarea.value.length < 20) {
+    commErr.style.display = "unset";
+    } else {
+        commErr.style.display = "none";
+  }
+};
+
+
 function testErrN() {
   if (iName.value.length >= 3) {
     unShowNameErr();
   } else {
     showNameErr();
   }
-}
+};
 
 function showNameErr() {
   if (iName.value.length <= 3) {
     nameErr.style.display = "unset";
   }
-}
+};
 
 function unShowNameErr() {
   if (iName.value.length <= 3) {
     nameErr.style.display = "none";
   }
-}
+};
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
@@ -380,8 +62,10 @@ let feedbackFormData = {};
 function onFormInput(e) {
   e.preventDefault();
 
-  unShowNameErr();
-  testErrN();
+    unShowNameErr();
+    showCommentErr()
+    testErrN();
+    showTotalErr()
 
   feedbackFormData[e.target.name] = e.target.value;
 
@@ -394,7 +78,7 @@ function onFormInput(e) {
   if (e.target === textarea) {
     feedbackFormData.message = e.target.value;
   }
-}
+};
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -404,7 +88,7 @@ function onFormSubmit(e) {
   localStorage.removeItem(STORAGE_KEY);
 
   feedbackFormData = {};
-}
+};
 
 function textareaInputMessage() {
   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
@@ -418,7 +102,7 @@ function textareaInputMessage() {
     textarea.value = savedData.message;
     feedbackFormData.message = savedData.message;
   }
-}
+};
 
 function isBtnDisabled() {
   if (
@@ -431,7 +115,7 @@ function isBtnDisabled() {
   } else {
     submitButton.disabled = true;
   }
-}
+};
 
 function isBtnDisabledTest() {
   if (
@@ -442,7 +126,7 @@ function isBtnDisabledTest() {
   ) {
     return;
   }
-}
+};
 
 // ВАЛИДАЦИЯ ИМЕНИ
 iName.addEventListener('input', validateName);
@@ -456,7 +140,7 @@ function validateName() {
   } else {
     iName.setCustomValidity('');
   }
-}
+};
 
 // ВАЛИДАЦИЯ EMAIL
 iEmail.addEventListener('input', validateEmail);
@@ -470,7 +154,7 @@ function validateEmail() {
   } else {
     iEmail.setCustomValidity('');
   }
-}
+};
 
 // ВАЛИДАЦИЯ НОМЕРА
 iPhone.addEventListener('keydown', restrictNonNumericInput);
@@ -484,7 +168,7 @@ function restrictNonNumericInput(event) {
   if (!allowedKeys.includes(keyPressed)) {
     event.preventDefault();
   }
-}
+};
 
 function cleanNonNumericInput() {
   let phone = iPhone.value;
@@ -501,7 +185,7 @@ function validatePhone() {
   } else {
     iPhone.setCustomValidity('');
   }
-}
+};
 
 textareaInputMessage();
 
