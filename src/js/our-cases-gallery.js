e16041f0e35f0a7c5b41f7e2fffaa8b7081a02be
KolBa228 <technotrashinfo@gmail.com>
2023-07-06 21:54:20 +0100

    dasdasda

--- src/js/our-cases-gallery.js
+++ src/js/our-cases-gallery.js
@@ -3,35 +3,35 @@ export const galleryCases = [
     title: 'Hotel-restaurant',
     description:
       'Our web studio emphasizes the uniqueness and excellence of each project. This site reflects our immersive approach, blending virtual reality with real-world luxury and sophistication through interactive and elegant design',
-    largeImage: './images/mimino-hero.jpg',
-    smallImage: './images/mimino-start.jpg',
+    largeImage: '/images/mimino-hero.jpg',
+    smallImage: '/images/mimino-start.jpg',
   },
   {
     title: 'Find-pets',
     description:
       "A shining example of our professional expertise in design and development. We've crafted an efficient and intuitive interface that enables pet owners to quickly and effectively post information about their lost companions",
-    largeImage: './images/pets-hero.jpg',
-    smallImage: './images/pets-find.jpg',
+    largeImage: '/images/pets-hero.jpg',
+    smallImage: '/images/pets-find.jpg',
   },
   {
     title: 'My-kitchen',
     description:
       'We strive to create a virtual space where customers can enjoy a rich selection of high-quality furniture for their kitchens. Our site has a modern and elegant design that reflects the beauty and practicality of kitchen furnishings.',
-    largeImage: './images/kitchen-big.jpg',
-    smallImage: './images/kitchen-small.jpg',
+    largeImage: '/images/kitchen-big.jpg',
+    smallImage: '/images/kitchen-small.jpg',
   },
   {
     title: 'Tech-Shop',
     description:
       'Our website has a modern and stylish design that makes it easy for users to find the technology they need. We provide convenient searches, filters and detailed product descriptions to help customers make informed choices',
-    largeImage: './images/tech-big.jpg',
-    smallImage: './images/tech-small.jpg',
+    largeImage: '/images/tech-big.jpg',
+    smallImage: '/images/tech-small.jpg',
   },
   {
     title: 'Car-rental',
     description:
       'Our website has a modern and stylish design that makes it easy for users to find the technology they need. We provide convenient searches, filters and detailed product descriptions to help customers make informed choices',
-    largeImage: './images/car-big.jpg',
-    smallImage: './images/car-small.jpg',
+    largeImage: '/images/car-big.jpg',
+    smallImage: '/images/car-small.jpg',
   },
 ];
