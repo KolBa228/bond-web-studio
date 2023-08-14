506978d4ea0740632fbb8f5c92c49227bf5773d8
KolBa228 <technotrashinfo@gmail.com>
2023-08-14 22:04:20 +0100

    try php

--- src/index.php
+++ src/index.php
@@ -1,48 +1,23 @@
 <?php
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-//   $name = htmlspecialchars($_POST["firstname"]);
-// $phone = htmlspecialchars($_POST["phone"]);
-// $email = htmlspecialchars($_POST["email"]);
-// $comment = htmlspecialchars($_POST["subject"]);
-
-
-//   $message = "Name: " . $name . "\n";
-//   $message .= "Phone: " . $phone . "\n";
-//   $message .= "Email: " . $email . "\n";
-//   $message .= "Comment: " . $comment;
-
-//   $to = "bond.webstudio@gmail.com";
-//   $subject = "Contact Form Submission";
-
-//   if (mail($to, $subject, $message)) {
-//     echo "Email sent successfully!";
-//   } else {
-//     echo "Failed to send email.";
-//   }
-// }
-?>
-
-<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email_to = "bond.webstudio@gmail.com";
-  $name = htmlspecialchars($_POST["name"]);
+  $name = htmlspecialchars($_POST["firstname"]);
   $email_from = htmlspecialchars($_POST["email"]);
-  $message = htmlspecialchars($_POST["message"]);
+  $message = htmlspecialchars($_POST["subject"]);
   $email_subject = "Feedback from website";
   
-  $headers = "From: $email_from \r\n";
-  $headers .= "Reply-To: $email_from \r\n";
+  $headers = "From: $email_from\r\n";
+  $headers .= "Reply-To: $email_from\r\n";
   
-  $message_body = "Name: $name \r\nMessage: $message";
+  $message_body = "Name: $name\r\nMessage: $message";
   
   ini_set("sendmail_from", $email_from);
-  $sent = mail($email_to, $email_subject, $message_body, $headers, "-f" . $email_from);
+  $sent = mail($email_to, $email_subject, $message_body, $headers, "-f$email_from");
   
   if ($sent) {
-    header("Location: http://fasthosts.co.uk");
+    echo "Message sent successfully. Thank you!";
   } else {
-    echo "There has been an error sending your comments. Please try later.";
+    echo "Message delivery failed.";
   }
 }
 ?>
